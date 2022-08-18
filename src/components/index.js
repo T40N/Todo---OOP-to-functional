c4f9c36ec9addc51f594815c2b22cdbedda8806d
Bartosz Ratajczyk <Bartosz.Ratajczyk@itechart-group.com>
2022-08-18 14:23:38 +0200

    adding components imports to index.js

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Button } from "./Button";
+export { default as Form } from "./Form";
+export { default as Header } from "./Header";
+export { default as Input } from "./Input";
+export { default as Layout } from "./Layout";
+export { default as Task } from "./Task";
+export { default as TaskGroup } from "./TaskGroup";
+export { default as TaskList } from "./TaskList";
